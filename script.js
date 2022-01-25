79eaa003a1ba83a344399334e958a2e474a70b7e
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2022-01-26 03:20:01 +0530

    Date Updated (Final commit?)
    The date is updated on site.
    Maybe The Final commit.

---
+++ script.js
@@ -0,0 +1,23 @@
+let newYear = "1 Jan 2023"
+let secondsEl = document.getElementById("seconds")
+let minutesEl = document.getElementById("minutes")
+let hoursEl = document.getElementById("hours")
+let daysEl = document.getElementById("days")
+
+function getCount() {
+    const newYearsDate = new Date(newYear)
+    const currentDate = new Date();
+    
+    const time = (newYearsDate - currentDate) / 1000;
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor(time / 60 % 60);
+    const hours = Math.floor(time / 3600 % 24)
+    const days = Math.floor(time / 3600 / 24)
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+}
+
+setInterval(getCount, 1000);
